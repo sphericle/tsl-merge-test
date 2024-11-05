b3acac8ccd631cb247d5462712428b5c5d19fff2
sphericle <jackb2033@gmail.com>
2024-11-05 14:55:19 -0500

    lmao ok

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-import json
+import json, requests
 
 path = "repos/layout-list/data/"
 list_index_result = "_list.json"
 benchmark = "_"
+rank = 0
 
-list = json.parse(list_index_result)
+with open(path + list_index_result) as json_file:
+    list = json.load(json_file)
 
+for levelpath in list:
+    print(levelpath)
+    rank += 1
+    
+    with open(path + levelpath + '.json') as level_file:
+        level = json.load(level_file)
+    
+        level['rank'] = rank
+        data = {
+            'name': level.name,
+            'position': rank,
+            'requirement': level.percentToQualify,
+            'verifier': level.verifier,
+            'publisher': level.author,
+            'creators': level.creators,
+            'video': level.verification
+        }
+        
+        print(data)
+        
+        requests.post('https://127.0.0.1/api/v2/demons/', data=data,)
