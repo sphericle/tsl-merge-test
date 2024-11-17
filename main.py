b14f22359c5776006442579074e67e8c2f0bba24
sphericle <jackb2033@gmail.com>
2024-11-16 19:15:36 -0500

    daaaang

--- main.py
+++ main.py
@@ -5,27 +5,48 @@ list_index_result = '_list.json'
 benchmark = '_'
 rank = 0
 
-with open(path + list_index_result) as json_file:
-    list = json.load(json_file)
-
-for levelpath in list:
-    print(levelpath)
-    rank += 1
+initGet = requests.get('https://cscl.shuttleapp.rs/api/v2/demons/')
+response = initGet.content
+listdata = json.loads(response)
+minLevel = listdata[0]
+for minLevel in listdata:
+    fullReq = requests.get(f'https://cscl.shuttleapp.rs/api/v2/demons/{minLevel['id']}')
+    response2 = fullReq.content
+    wtfpointercrate = json.loads(response2)
+    level = wtfpointercrate['data']
+    print(level['name'])
     
-    with open(path + levelpath + '.json') as level_file:
-        level = json.load(level_file)
+    creators = []
+    records = []
     
-        level['rank'] = rank
-        data = {
-            'name': level['name'],
-            'position': rank,
-            'requirement': level['percentToQualify'],
-            'verifier': level['verifier'],
-            'publisher': level['author'],
-            'creators': level['creators'],
-            'video': level['verification']
+    for creator in level['creators']:
+        creators.append(creator['name'])
+        
+    for record in level['records']:
+        recordBody = {
+            'user': record['player']['name'],
+            'link': record['video'],
+            'percent': record['progress'],
+            'hz': 240,
+            'enjoyment': record['enjoyment']
         }
+        records.append(recordBody)
         
-        print(data)
-        
-        requests.post('https://127.0.0.1:8001/api/v2/demons/', data=data,)
+    body = {
+        'id': level['id'],
+        'name': level['name'],
+        'author': level['publisher']['name'], # python the goat!!!
+        'creators': creators,
+        'verifier': level['verifier'],
+        'verification': level['video'],
+        'percentToQualify': level['requirement'],
+        'song': 'wip',
+        'songLink': 'wip2',
+        'difficulty': 1,
+        'records': records
+    }
+    
+    with open('repos/layout-list/data/' + body['name'] + '.json', 'w') as f:
+        f.write(json.dumps(body))
+        f.close()
+        print('done')
